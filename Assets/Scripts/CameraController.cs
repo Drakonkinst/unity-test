129379f7e736214bd535ccd45c34d85fa79c9f8f
leauxgan1 <logan.langdon@yahoo.com>
2020-07-02 15:02:24 -0700

    Added camera following player script
    - Cube object now has the player tag
    - Added script to make the position equal to the player's movement with an offset.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject playerCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCharacter = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = (playerCharacter.transform.position + new Vector3(0,3,-10));
+    }
+}
